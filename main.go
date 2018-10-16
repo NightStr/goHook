9e79c8c2194e4c260a21f520a68b66941ab79e0f
strix <p.smagin@napoleonit.ru>
2018-10-16 10:29:34 +0500

    config through the env

--- main.go
+++ main.go
@@ -4,10 +4,10 @@ import (
 	"fmt"
 	"github.com/Syfaro/telegram-bot-api"
 	"github.com/gorilla/mux"
-	"gopkg.in/yaml.v2"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 	"strconv"
 )
 
@@ -17,29 +17,13 @@ type Config struct {
 	PORT string
 }
 
-func loadConfing() Config {
-	config := Config{}
-	data, err := ioutil.ReadFile("./config.yaml")
-
-	if err != nil {
-		log.Panic(err)
-	}
-
-	err = yaml.Unmarshal(data, &config)
-
-	if err != nil {
-		log.Panic(err)
-	}
-	return config
-}
-
-func telegramBotResponding(bot *tgbotapi.BotAPI, updates tgbotapi.UpdatesChannel, config Config) {
+func telegramBotResponding(bot *tgbotapi.BotAPI, updates tgbotapi.UpdatesChannel) {
 	for update := range updates {
 		if update.Message == nil { // ignore any non-Message Updates
 			continue
 		}
 		if "/get_url" == update.Message.Text {
-			messageText := config.HOST + strconv.FormatInt(update.Message.Chat.ID, 10) + "/"
+			messageText := os.Getenv("HOST_NAME") + strconv.FormatInt(update.Message.Chat.ID, 10) + "/"
 			msg := tgbotapi.NewMessage(update.Message.Chat.ID, messageText)
 			bot.Send(msg)
 		}
@@ -68,25 +52,25 @@ func makeHandler(bot *tgbotapi.BotAPI) func(resp http.ResponseWriter, req *http.
 }
 
 func main() {
-	config := loadConfing()
-	var bot, err = tgbotapi.NewBotAPI(config.KEY)
+	var bot, err = tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_KEY"))
+	bot.Debug = true
 	if err != nil {
 		log.Panic(err)
 	}
-	router := mux.NewRouter()
-
-	router.HandleFunc("/{chatId:\\d+}/", makeHandler(bot)).Methods("POST")
-
 	u := tgbotapi.NewUpdate(0)
 	u.Timeout = 60
 
 	updates, err := bot.GetUpdatesChan(u)
+	go telegramBotResponding(bot, updates)
+
+	router := mux.NewRouter()
+	router.HandleFunc("/{chatId:\\d+}/", makeHandler(bot)).Methods("POST")
 
 	if err != nil {
 		log.Panic(err)
 	}
+	port := os.Getenv("PORT")
 
-	go telegramBotResponding(bot, updates, config)
 	http.Handle("/", router)
-	http.ListenAndServe(":"+config.PORT, nil)
+	http.ListenAndServe(":"+port, nil)
 }
